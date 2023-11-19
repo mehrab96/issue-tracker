75c8128b3934215a21f7a0913ca04f4aa39fc6e1
mehrab esmailnia <mehrabesmailnia96@gmail.com>
2023-11-19 21:05:12 +0330

    Build a new issue page

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,16 @@
+import { Button, TextArea, TextFieldInput, TextFieldRoot } from '@radix-ui/themes'
+import React from 'react'
+
+const NewIssuePage = () => {
+  return (
+    <div className='grid gap-2 grid-cols-1 max-w-xl'>
+        <TextFieldRoot>
+            <TextFieldInput placeholder='title'></TextFieldInput>
+        </TextFieldRoot>
+        <TextArea placeholder='description'></TextArea>
+        <Button className='w-40'>Submit New Issue</Button>
+    </div>
+  )
+}
+
+export default NewIssuePage
